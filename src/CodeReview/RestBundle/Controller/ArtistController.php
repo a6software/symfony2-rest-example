914fa3f787957067c48228ea7502bf4f0b22eb38
Code Review <codereview@a6chris.local>
2014-11-10 12:06:25 +0000

    added test dump

--- src/CodeReview/RestBundle/Controller/ArtistController.php
+++ src/CodeReview/RestBundle/Controller/ArtistController.php
@@ -6,8 +6,5 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ArtistController extends Controller
 {
-    public function indexAction($name)
-    {
-        return $this->render('CodeReviewRestBundle:Default:index.html.twig', array('name' => $name));
-    }
+
 }
--- tests/api/CodeReview/RestBundle/Controller/ArtistControllerCest.php
+++ tests/api/CodeReview/RestBundle/Controller/ArtistControllerCest.php
@@ -18,7 +18,7 @@ class ArtistControllerCest
         $i->wantTo('ensure Getting an invalid Artist id returns a 404 code');
 
         $i->sendGET('/api/artists/23623623.json');
-        $i->seeResponseCodeIs(200);
+        $i->seeResponseCodeIs(404);
         $i->seeResponseIsJson();
     }
 
@@ -40,5 +40,14 @@ class ArtistControllerCest
         ));
     }
 
+    public function getSecondValidArtist(ApiGuy $i)
+    {
+        $i->sendGET('/api/artists/2.json');
+        $i->seeResponseCodeIs(200);
+        $i->seeResponseIsJson();
 
+        $i->seeResponseContains(array(
+            'name'  => 'The Rolling Stones',
+        ));
+    }
 }
