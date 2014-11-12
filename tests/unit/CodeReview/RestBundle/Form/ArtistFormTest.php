3aa9ccc6e9a2b17118622b5f889957bcfc47da8f
Code Review <codereview@a6chris.home>
2014-11-12 12:23:49 +0000

    artist form unit test

---
+++ tests/unit/CodeReview/RestBundle/Form/ArtistFormTest.php
@@ -0,0 +1,40 @@
+<?php
+
+class ArtistFormTest extends Symfony\Component\Form\Test\TypeTestCase
+{
+    protected function setUp()
+    {
+        parent::setUp();
+    }
+
+    protected function tearDown()
+    {
+    }
+
+    // tests
+    public function testSubmitValidData()
+    {
+        $formData = array(
+            'name' => 'test',
+        );
+
+        $type = new \CodeReview\RestBundle\Form\ArtistType();
+        $form = $this->factory->create($type);
+
+        $object = new \CodeReview\RestBundle\Entity\Artist();
+        $object->setName($formData['name']);
+
+        // submit the data to the form directly
+        $form->submit($formData);
+
+        $this->assertTrue($form->isSynchronized());
+        $this->assertEquals($object, $form->getData());
+
+        $view = $form->createView();
+        $children = $view->children;
+
+        foreach (array_keys($formData) as $key) {
+            $this->assertArrayHasKey($key, $children);
+        }
+    }
+}
